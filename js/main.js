86425230d8331aa246db01bbfedece7486fe8079
Josh <josh.westbury@saascg.com>
2021-09-02 08:42:41 -0500

    update main.js with new scrolling titles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-var typed = new Typed(".element", {
-  strings: [
-    "Software Developer",
-    "Linguist",
-    "Quick Learner",
-    "Coffee Fanatic",
-    "Self-Starter"
-  ],
-  typeSpeed: 100,
-  smartBackspace: true,
-  backSpeed: 80,
-  loop: true
+var typed = new Typed('.element', {
+    strings: [
+        'NetSuite Developer',
+        'Integrations Architect',
+        'Linguist',
+        'Coffee Fanatic',
+    ],
+    typeSpeed: 100,
+    smartBackspace: true,
+    backSpeed: 80,
+    loop: true,
 });
